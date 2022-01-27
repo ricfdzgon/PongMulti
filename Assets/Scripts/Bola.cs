4120793c79ee8e68969e3e3177d2d81a1c3cbe33
Ricardo Fernández <ricfdzgon@gmail.com>
2022-01-27 20:19:47 +0100

    Detectar bola

--- Assets/Scripts/Bola.cs
+++ Assets/Scripts/Bola.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Bola : MonoBehaviour
 {
     public float velocidad = 30.0f;
     // Start is called before the first frame update
+
     void Start()
     {
         GetComponent<Rigidbody2D>().velocity = Vector2.right * velocidad;
